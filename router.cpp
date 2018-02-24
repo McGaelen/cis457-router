409439bda8b5933762c74301a5148287ab389f8d
None <None>
2018-02-23 17:21:24 -0800

    almost finished

--- router.cpp
+++ router.cpp
@@ -3,20 +3,21 @@
 #include <net/ethernet.h>
 #include <stdio.h>
 #include <errno.h>
-#include <sys/types.h>
+//#include <sys/types.h>
 #include <ifaddrs.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/select.h>
-#include <linux/if_ether.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <sys/select.h>
+//#include <linux/if_ether.h>
 #include <netinet/ether.h>
 #include <stdlib.h>
-#include <net/ethernet.h>
+//#include <net/ethernet.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
-#include <net/if_arp.h>
+//#include <net/if_arp.h>
+#include <netinet/ether.h>
 
 
 int main() {
@@ -34,6 +35,7 @@ int main() {
         u_int8_t mac[6];
         struct in_addr ip;
         char interfaceName[30];
+        int fileDescriptor;
     };
     struct addressPair addresses[4];
 
@@ -97,6 +99,7 @@ int main() {
                     perror("socket");
                     return 2;
                 }
+                addresses[atoi(&interfaceNumber)].fileDescriptor = packet_socket;
                 //Bind the socket to the address, so we only get packets
                 //recieved on this specific interface. For packet sockets, the
                 //address structure is a struct sockaddr_ll (see the man page
@@ -158,8 +161,11 @@ int main() {
                 struct ether_header *ether;
                 ether = (struct ether_header *)packet;
 
-                // Get the destination IP and figure out if it's ours
+                // Get the destination IP and figure out if it's ours --------------------------------------------------
+                // If it's ARP, grab the destIP from the ARP data
+                printf("about to check arp\n");
                 if (htons(sourceAddr.sll_protocol) == ETH_P_ARP) {
+                    printf("arp packet\n");
                     struct ether_arp *arp;
                     arp = (struct ether_arp *)(packet + sizeof(struct ether_header));
 
@@ -181,6 +187,7 @@ int main() {
                             break;
                         }
                     }
+                // If it's an IP packet, grab the destIP off the IP header
                 } else if (htons(sourceAddr.sll_protocol) == 0x800) {
                     struct ip *ipHeader;
                     ipHeader = (struct ip *)(packet + sizeof(struct ether_header) );
@@ -200,13 +207,13 @@ int main() {
                         }
                     }
                 }
+                // -----------------------------------------------------------------------------------------------------
 
-                // If the destination is meant for us, then handle the packet
+                // If the destination is meant for us, then handle the packet ------------------------------------------
                 if (isOurIp) {
                     if (htons(sourceAddr.sll_protocol) == ETH_P_ARP) {
                         // do arp stuff
                         printf("ARP PACKET!!!\n");
-
                         
                         struct ether_arp *arp;
                         arp = (struct ether_arp *)(packet + sizeof(struct ether_header));
@@ -247,14 +254,13 @@ int main() {
                         memcpy(&(spa.s_addr), arp->arp_spa, 4);
                         memcpy(&(tpa2.s_addr), arp->arp_tpa, 4);
 
-                        send(i, packet, packetSize, NULL);
+                        send(i, packet, packetSize, 0);
 
                     } else if (htons(sourceAddr.sll_protocol) == 0x800) {
-
+                        // Need to figure out which protocol this IP packet is for
                         struct ip *ipHeader;
                         struct icmp *icmpHeader;
 
-                        
                         ipHeader = (struct ip *)(packet + sizeof(struct ether_header) );
 
                         // if it's an ICMP packet
@@ -273,12 +279,13 @@ int main() {
 
                             icmpHeader->icmp_type = 0;
 
-                            send(i, packet, packetSize, NULL);
+                            send(i, packet, packetSize, 0);
                         }
                     }
-                // If it was not meant for us, then forward the packet
+
+                // If it was not meant for us, then forward the packet -------------------------------------------------
                 } else {
-                    // not our shit
+                    // Look up IP address in routing table -------------
                     char filename[50];
                     strcpy(filename, ourInterfaceName);
                     strcat(filename, "-table.txt");
@@ -288,7 +295,6 @@ int main() {
                     strcpy(destinationIpStr, inet_ntoa(destinationIp));
                     char line[50];
                     char interface[50];
-                    rewind(f);
                     while (fgets(line, 50, f) != NULL) {
                         if (strncmp(destinationIpStr, line, 6) == 0) {
                             // we found the matching entry for the destination IP
@@ -298,6 +304,8 @@ int main() {
                     }
                     fclose(f);
 
+                    // Generate ARP Request -------------
+                    // Constructing ethernet header on ARP Request
                     char arpRequest[1500];
                     struct ether_header *newEtherHeader;
                     newEtherHeader = (struct ether_header *)arpRequest;
@@ -318,17 +326,53 @@ int main() {
                     memcpy(newEtherHeader->ether_dhost, &broadcastAddr, 6);
                     memcpy(&(newEtherHeader->ether_type), &arpProtocol, 2);
 
-                    newArpHeader->ea_hdr.ar_hrd = 1;
-                    newArpHeader->ea_hdr.ar_pro = 0x800;
-                    newArpHeader->ea_hdr.ar_hln = 6;
-                    newArpHeader->ea_hdr.ar_pln = 4;
-                    newArpHeader->ea_hdr.ar_op = ARPOP_REQUEST;
+                    // Constructing ARP Header on ARP Request
+                    newArpHeader->ea_hdr.ar_hrd = htons(1);
+                    newArpHeader->ea_hdr.ar_pro = htons(0x800);
+                    newArpHeader->ea_hdr.ar_hln = htons(6);
+                    newArpHeader->ea_hdr.ar_pln = htons(4);
+                    newArpHeader->ea_hdr.ar_op = htons(ARPOP_REQUEST);
                     memcpy(newArpHeader->arp_sha, newEtherHeader->ether_shost, 6);
-                    memcpy(newArpHeader->arp_spa, addresses[addressIndex].ip.s_addr, 4);
+                    memcpy(newArpHeader->arp_spa, &(addresses[addressIndex].ip.s_addr), 4);
                     u_int64_t zeroes = 0x0;
-                    memcpy(newArpHeader->arp_tha, zeroes, 6);
-                    // memcpy(newArpHeader->tpa, ) how to find next IP in chain?
-                    
+                    memcpy(newArpHeader->arp_tha, &(zeroes), 6);
+
+                    if (line[12] == '-') {
+                        memcpy(newArpHeader->arp_tpa, &(destinationIp.s_addr), 4);
+                    } else {
+                        struct in_addr *arpDestination;
+                        inet_aton(line+12, arpDestination);
+                        memcpy(newArpHeader->arp_tpa, &(arpDestination->s_addr), 4);
+                    }
+
+                    // Find the correct socket to send the ARP request on
+                    int arpSocket = 0;
+                    int arpSocketIndex = 0;
+                    for (int z = 0; z < 4; z++) {
+                        if (strncmp(interface, addresses[z].interfaceName, 7) == 0) {
+                            arpSocket = addresses[z].fileDescriptor;
+                            arpSocketIndex = z;
+                            break;
+                        }
+                    }
+
+                    // send Arp request
+                    send(arpSocket, arpRequest, 42, 0);
+
+                    // Reveive arp response
+                    char arpResponse[50];
+                    int arpResponseSize = recv(i, arpResponse, 50, MSG_WAITALL);
+
+                    // Add MAC to original packet
+                    struct ether_arp *arpResponseStruct;
+                    arpResponseStruct = (struct ether_arp *)(arpResponse + sizeof(struct ether_header));
+                    memcpy(ether->ether_shost, addresses[arpSocketIndex].mac, 6);
+                    memcpy(ether->ether_dhost, arpResponseStruct->arp_sha, 6);
+                    printf("orig packet shost: %s\n", ether_ntoa((struct ether_addr *)ether->ether_shost));
+                    printf("orig packet dhost: %s\n", ether_ntoa((struct ether_addr *)ether->ether_dhost));
+
+                    // Forward original packet
+                    send(arpSocket, packet, packetSize, 0);
                 }
             }
         }
